1324ca5f95f35dae4da41269f7d153ed3331125c
Ian Korpi <iankorpi@outlook.com>
2021-06-03 16:34:49 +0000

    Add test-plugin as an example.
    Add test-plugin as an example of a wordpress plugin.

---
+++ plugins/test-plugin.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: TestPluginShortcode
+Plugin URI: www.example.com
+Version: 1.0.0
+*/
+
+/* The above comment block identifies the plugin to WordPress. */
+
+
+function get_shortcode_example_shortcode() {
+/* Shortcode functions should return some HTML:  */
+  return "<p>This is a test of the shortcode plugin.</p>";
+}
+
+function add_notice_shortcode_plugin_active() {
+/* Adds a notice to the admin control panel (this will appear on every page of the dashboard)
+informing that the plugin is active: */
+  echo("<DIV class='notice-info'><p>The shortcode plugin is currently active.</DIV>");
+}
+
+/* Register the shortcode using add_shortcode. In this case, 'test_shortcode' is our shortcode.
+It will replace [test_shortcode] in our posts with the code returned by the above function. */
+add_shortcode('test_shortcode', 'get_shortcode_example_shortcode');
+/* Register the notice function to print using the admin_notices hook. */
+add_action( 'admin_notices', 'add_notice_shortcode_plugin_active');
+?>
