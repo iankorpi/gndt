0204b1d195d04f4bcf818c563b7dc79fe346a87d
Ian Korpi <iankorpi@outlook.com>
2021-06-03 16:48:34 +0000

    Add prefixes to functions.

--- plugins/test-plugin.php
+++ plugins/test-plugin.php
@@ -8,12 +8,12 @@ Version: 1.0.0
 /* The above comment block identifies the plugin to WordPress. */
 
 
-function get_shortcode_example_shortcode() {
+function testgndt_get_shortcode_example_shortcode() {
 /* Shortcode functions should return some HTML:  */
   return "<p>This is a test of the shortcode plugin.</p>";
 }
 
-function add_notice_shortcode_plugin_active() {
+function testgndt_add_notice_shortcode_plugin_active() {
 /* Adds a notice to the admin control panel (this will appear on every page of the dashboard)
 informing that the plugin is active: */
   echo("<DIV class='notice-info'><p>The shortcode plugin is currently active.</DIV>");
@@ -21,7 +21,8 @@ informing that the plugin is active: */
 
 /* Register the shortcode using add_shortcode. In this case, 'test_shortcode' is our shortcode.
 It will replace [test_shortcode] in our posts with the code returned by the above function. */
-add_shortcode('test_shortcode', 'get_shortcode_example_shortcode');
+add_shortcode('test_shortcode', 'testgndt_get_shortcode_example_shortcode');
 /* Register the notice function to print using the admin_notices hook. */
-add_action( 'admin_notices', 'add_notice_shortcode_plugin_active');
+add_action( 'admin_notices', 'testgndt_add_notice_shortcode_plugin_active');
 ?>
+
